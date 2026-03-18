6046c3c060e94f47276843dc84525eac2eb28ae2
Andrés Podadera <andrespodadera87@gmail.com>
2026-03-18 16:24:34 +0100

    Uploading build to deploy in Github Pages

--- 05-heroes-app/vite.config.ts
+++ 05-heroes-app/vite.config.ts
@@ -7,7 +7,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/CursoReact2025/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
